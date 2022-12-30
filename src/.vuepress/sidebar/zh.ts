2342e68f3d72119ceb48990dc5da702870e732cd
yms <sxpyywx@126.com>
2022-12-30 09:54:15 +0800

    new file:   .github/workflows/deploy-docs.yml 	new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/.vuepress/config.ts 	new file:   src/.vuepress/navbar/index.ts 	new file:   src/.vuepress/navbar/zh.ts 	new file:   src/.vuepress/public/assets/icon/apple-icon-152.png 	new file:   src/.vuepress/public/assets/icon/chrome-192.png 	new file:   src/.vuepress/public/assets/icon/chrome-512.png 	new file:   src/.vuepress/public/assets/icon/chrome-mask-192.png 	new file:   src/.vuepress/public/assets/icon/chrome-mask-512.png 	new file:   src/.vuepress/public/assets/icon/guide-maskable.png 	new file:   src/.vuepress/public/assets/icon/guide-monochrome.png 	new file:   src/.vuepress/public/assets/icon/ms-icon-144.png 	new file:   src/.vuepress/public/favicon.ico 	new file:   src/.vuepress/public/logo.png 	new file:   src/.vuepress/public/logo.svg 	new file:   src/.vuepress/sidebar/index.ts 	new file:   src/.vuepress/sidebar/zh.ts 	new file:   src/.vuepress/styles/config.scss 	new file:   src/.vuepress/styles/index.scss 	new file:   src/.vuepress/styles/palette.scss 	new file:   src/.vuepress/theme.ts 	new file:   src/README.md 	new file:   src/demo/README.md 	new file:   src/demo/disable.md 	new file:   src/demo/encrypt.md 	new file:   src/demo/markdown.md 	new file:   src/demo/page.md 	new file:   src/intro.md 	new file:   src/posts/apple/1.md 	new file:   src/posts/apple/2.md 	new file:   src/posts/apple/3.md 	new file:   src/posts/apple/4.md 	new file:   src/posts/banana/1.md new file:   src/posts/banana/2.md 	new file:   src/posts/banana/3.md 	new file:   src/posts/banana/4.md 	new file:   src/posts/cherry.md 	new file:   src/posts/dragonfruit.md 	new file:   src/posts/strawberry.md 	new file:   src/posts/tomato.md

---
+++ src/.vuepress/sidebar/zh.ts
@@ -0,0 +1,15 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export const zhSidebar = sidebar({
+  "/": [
+    "",
+    {
+      text: "文章",
+      icon: "note",
+      prefix: "posts/",
+      children: "structure",
+    },
+    "intro",
+    "slides",
+  ],
+});
