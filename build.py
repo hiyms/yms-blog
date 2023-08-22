541251a43d11364ae499d5cb4a7bcc6713f01568
yms <369012270@qq.com>
2023-08-22 22:52:10 +0800

    modified:   build.py

--- build.py
+++ build.py
@@ -7,9 +7,10 @@ LOGO = r"https://file.yms.tdrweb.top/img/me.png"
 @log.catch(level="ERROR")
 def main():
     log.info("启动")
+    assert os.system("npm run build")
+    log.success("构建成功")
     os.remove("./public/img/avatar.png")
     wget.download(LOGO,"./public/img/avatar.png")
     log.success("成功替换图片")
-    assert os.system("npm run build")
-    log.success("成功")
+    
 main()
